d87d7d80737bf265a9fd77cf6eaf7e288a5bde30
eduardotrandafilov <eduardo.trandafilov@gmail.com>
2019-09-01 20:00:50 -0300

    Implementaçao de log , validacao projeto e inclusao do nodemon

--- index.js
+++ index.js
@@ -4,7 +4,43 @@ const server = express();
 
 server.use(express.json());
 
+let numberOfRequests = 0;
 const projects = [];
+
+/**
+ * Middleware que dá log no número de requisições
+ */
+
+function logRequests(req, res, next) {
+  numberOfRequests++;
+  console.log(`Número de requisições:${numberOfRequests}`);
+  return next();
+}
+
+server.use(logRequests);
+
+/**
+ * Middleware que checa se o projeto existe
+ */
+
+function checkProject(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ message: "Project not found" });
+  }
+
+  return next();
+}
+
+/**
+ * Projects
+ */
+
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
 
 server.post("/projects", (req, res) => {
   const { id, title } = req.body;
@@ -20,11 +56,7 @@ server.post("/projects", (req, res) => {
   return res.json(projects);
 });
 
-server.get("/projects", (req, res) => {
-  return res.json(projects);
-});
-
-server.put("/projects/:id", (req, res) => {
+server.put("/projects/:id", checkProject, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
@@ -35,7 +67,7 @@ server.put("/projects/:id", (req, res) => {
   return res.json(project);
 });
 
-server.delete("/projects/:id", (req, res) => {
+server.delete("/projects/:id", checkProject, (req, res) => {
   const { id } = req.params;
   const index = projects.findIndex(p => p.id == id);
 
@@ -44,7 +76,11 @@ server.delete("/projects/:id", (req, res) => {
   return res.send();
 });
 
-server.post("/projects/:id/tasks", (req, res) => {
+/**
+ * Tasks
+ */
+
+server.post("/projects/:id/tasks", checkProject, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   const project = projects.find(p => p.id == id);
